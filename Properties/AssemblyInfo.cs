9e7f2da9c89605d178da8b9ebbdce2d266d4226f
Claudio Sanchez <claudio@megsoftconsulting.com>
2015-12-15 20:46:43 -0500

    Initial Commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("ManifestEditor")]
+[assembly: AssemblyDescription("Manifest Editor command line tool")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Megsoft Consulting, Inc")]
+[assembly: AssemblyProduct("ManifestEditor")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("4ca7eb5a-0d12-47a5-ac44-df6f50be1629")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
